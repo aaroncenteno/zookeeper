66f0414e0cae39a75ae5623d6b681a502749a3e4
ASC8 <asamcent@gmail.com>
2020-10-15 15:20:05 -0700

    integrate front end

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -1,3 +1,5 @@
+// const { query, response } = require("express");
+
 const $animalForm = document.querySelector('#animals-form');
 const $displayArea = document.querySelector('#display-area');
 
@@ -30,7 +32,17 @@ const getAnimals = (formData = {}) => {
   });
 
   console.log(queryUrl);
-
+  fetch(queryUrl)
+    .then(response => {
+      if(!response.ok){
+        return alert('Error: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(animalData => {
+      console.log(animalData);
+      printResults(animalData);
+    });
 };
 
 const handleGetAnimalsSubmit = event => {
